f9139c3027c946ba37648608e05a5b74f3e9e48a
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-10-25 20:20:50 +0100

    Parse JSON safely
    If the JSON comes with any sort of errors that make it unparsable,
    an exception will be thrown and the app will be stopped. It is then
    recommended to always try/catch any JSON parsing.

--- spec/popolo-spec.js
+++ spec/popolo-spec.js
@@ -7,4 +7,14 @@ describe('Popolo', function() {
     expect(popolo.persons[0].id).toEqual('123');
     expect(popolo.persons[0].name).toEqual('Jon Doe');
   });
+
+  it('returns no data if there is a parsing error', function() {
+    var popolo = Popolo.read('spec/fixtures/bad.json');
+    expect(popolo).toEqual({});
+  });
+
+  it('returns no data if file does not exits', function() {
+    var popolo = Popolo.read('foo');
+    expect(popolo).toEqual({});
+  });
 });
