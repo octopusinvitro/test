dac20229c3235a0d45028db14119d698cf20a237
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-04-01 17:00:53 +0100

    wIP

---
+++ spec/popolo/persons-spec.js
@@ -0,0 +1,26 @@
+describe('Persons', function() {
+  var
+    data   = loadFixture('spec/fixtures/ep-popolo-v1.0.json'),
+    Popolo = require('../../index').Popolo,
+    popolo;
+
+  beforeEach(function() {
+    popolo = new Popolo();
+  });
+
+  it('reads a Popolo json file', function() {
+    var file = popolo.read(data);
+    expect(file.persons.length).toEqual(1);
+    expect(file.persons[0].id).toEqual('123');
+    expect(file.persons[0].name).toEqual('Jon Doe');
+  });
+
+  it('returns no data if there is a parsing error', function() {
+    var file = popolo.read('{"hey"}');
+    expect(file).toEqual({});
+  });
+
+  function loadFixture(path) {
+    return require('fs').readFileSync(path, 'utf-8');
+  }
+});
