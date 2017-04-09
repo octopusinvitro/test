45198473dfbcfb6eaed887d336965aad81537fea
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-04-09 20:20:23 +0100

    Return a PopoloJSON from Popolo containing an instance of Areas

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 'use strict';
 
-var fs, Popolo;
+var
+  fs, Popolo, PopoloJSON,
+  Areas;
 
 fs = require('fs');
+Areas = require('./src/areas');
+
 Popolo = Popolo || {};
+
+PopoloJSON = function(popolo) {
+  this.popolo = popolo;
+  this.areas = new Areas(popolo.areas, this);
+  this.memberships = popolo.memberships;
+  this.persons = popolo.persons;
+};
 
 Popolo.read = function(path) {
   try {
-    return JSON.parse(fs.readFileSync(path, 'utf-8'));
+    var popolo = JSON.parse(fs.readFileSync(path, 'utf-8'));
+    return new PopoloJSON(popolo);
   } catch (e) {
     return {};
   }
--- spec/popolo-spec.js
+++ spec/popolo-spec.js
@@ -1,22 +1,22 @@
 'use strict';
 
 describe('Popolo', function() {
-  var Popolo = require('../index');
+  var Popolo = require('../index'), popolo;
 
   it('reads a Popolo json file', function() {
-    var popolo = Popolo.read('spec/fixtures/dummy-person.json');
+    popolo = Popolo.read('spec/fixtures/dummy-person.json');
     expect(popolo.persons.length).toEqual(1);
     expect(popolo.persons[0].id).toEqual('123');
     expect(popolo.persons[0].name).toEqual('Jon Doe');
   });
 
   it('returns no data if there is a parsing error', function() {
-    var popolo = Popolo.read('spec/fixtures/bad.json');
+    popolo = Popolo.read('spec/fixtures/bad.json');
     expect(popolo).toEqual({});
   });
 
   it('returns no data if file does not exits', function() {
-    var popolo = Popolo.read('foo');
+    popolo = Popolo.read('foo');
     expect(popolo).toEqual({});
   });
 });
